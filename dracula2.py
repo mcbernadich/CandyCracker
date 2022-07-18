7310db1bc5d59025ef592c747877a7505b4b818d
Miquel Colom i Bernadich <mcbernadich@mpifr-bonn.mpg.de>
2022-07-18 21:47:36 +0200

    Fixed typo

--- dracula2.py
+++ dracula2.py
@@ -53,7 +53,7 @@ def add_jumps_and_fit(parFile,timFile,skipJumps,nFits):
 	print("Reading ToAs and computing jumps.")
 	par_write.write("\n")
 	for line in tim_read:
-		if line=="FORMAT 1\n" or line=="MODE 1\n" or line[1]="C":
+		if line=="FORMAT 1\n" or line=="MODE 1\n" or line[1]=="C":
 			dummy=1
 		else:
 			line=line.strip().split()
