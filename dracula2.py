1810dd88d04054b8ad4ef1027e2776891c4b2ef6
Miquel Colom i Bernadich <mcbernadich@mpifr-bonn.mpg.de>
2022-09-27 14:13:09 +0200

    Added position prior option.

--- dracula2.py
+++ dracula2.py
@@ -60,6 +60,9 @@ def compatible(parFile,ref_pos,tol):
 	solution = SkyCoord(ra, dec, unit=(u.hourangle, u.deg), frame='icrs')
 	reference = SkyCoord(ref_pos.split(",")[0], ref_pos.split(",")[1], unit=(u.hourangle, u.deg), frame='icrs')
 
+	print(solution.arcsec)
+	print(reference.arcsec)
+
 #	distance = solution.separation(reference)
 
 	if ( abs(solution.arcsec[0]-reference.arcsec[0])-dra > tolerance/np.cos(reference.rad[0]) ) or ( abs(solution.arcsec[1]-reference.arcsec[1])-ddec > tolerance ):
@@ -568,6 +571,9 @@ parser.add_argument("--pre_fits",type=int,help="Number of fits done to the initi
 parser.add_argument("--par_with_jumps",type=bool,help="If set, then jumps are assumed to be added manually and they are are not added by dracula2. Make sure that they are in the correct format!",default=False)
 parser.add_argument("--up_to_jump",type=int,help="If set, then skip the run will end after removing this jump (counted from 0). Useful for splicing runs.")
 parser.add_argument("--skip_jumps",type=int,help="If set, then skip the removal of this many jumps. Useful for continuing a broken run. MUST BE USED WITH --par_with_jumps",default=0)
+parser.add_argument("--position_prior",help="Prior position of the pulsar. Solutions with a different position will be deleted. It must be used --position_tolerance")
+parser.add_argument("--position_tolerance",type=float,help="Positional tolerance of found positions. It must be used with --position_prior.")
+
 args = parser.parse_args()
 
 parFile=args.parameter
@@ -614,11 +620,11 @@ while i<=max_jump:
 		print("")
 		print("Surviving PAR files from removing the previous jump:",parFiles)
 
-		if (args.position_prior and args.postion_tolerance):
+		if (args.position_prior and args.position_tolerance):
 
 			for file in parFiles:
 
-				if compatible(file,args.position_prior,args.postion_tolerance)==False:
+				if compatible(file,args.position_prior,args.position_tolerance)==False:
 
 					#subprocess.run(["rm",file],stdout=subprocess.DEVNULL)
 					print("This one would be removed.")
